f90fe4fe8e3d5a72bdf36457504d20358ff796ae
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2020-07-13 21:59:16 -0500

    Added ID and reformatted script terminal outputs for easier reading

--- github-check.py
+++ github-check.py
@@ -41,13 +41,12 @@ if __name__ == "__main__":
                 is_success = False
             else:
                 # print(results_json)
-                print(f"Page count: {page_nbr}")
-                print(f"Record count: {len(results_json)}")                 
-                print(f"Status code: {results.status_code}")
+                print(f"Status: {results.status_code}. Page {page_nbr}: {len(results_json)} records.")
 
                 for result in results_json:
                     language = result["language"]   
                     name = result["name"] 
+                    id = result["id"]
 
                     if "-000" in name:
                         source = "Flatiron"
@@ -57,9 +56,9 @@ if __name__ == "__main__":
                     isForked = result["fork"]
 
                     if repos.get(language) is None:
-                        repos[language] = [{"name":name,"isForked":isForked,"source":source}]
+                        repos[language] = [{"name":name, "id":id, "isForked":isForked, "source":source}]
                     else:
-                        repos[language].append({"name":name,"isForked":isForked,"source":source})
+                        repos[language].append({"name":name, "id":id, "isForked":isForked, "source":source})
 
                 page_nbr += 1
     original_stdout = sys.stdout
@@ -75,10 +74,8 @@ if __name__ == "__main__":
 
 ## OAuth help?
 ## https://stackoverflow.com/questions/17622439/how-to-use-github-api-token-in-python-for-requesting
-## code from old version
-                # created_at = result["created_at"]
-                # description = result["description"]
-                # pushed_at = result["pushed_at"]
 
-                # if ((name.find('-000') == -1) and (language is not None)):
-                #     print(f"{name}\nCreated: {created_at}\nDescription: {description}\nLanguage: {language}\nPushed: {pushed_at}\nFork: {fork}\n")
+
+## keys for returned data
+# dict_keys(['id', 'node_id', 'name', 'full_name', 'private', 'owner', 'html_url', 'description', 'fork', 'url', 'forks_url', 'keys_url', 'collaborators_url', 'teams_url', 'hooks_url', 'issue_events_url', 'events_url', 'assignees_url', 'branches_url', 'tags_url', 'blobs_url', 'git_tags_url', 'git_refs_url', 'trees_url', 'statuses_url', 'languages_url', 'stargazers_url', 'contributors_url', 'subscribers_url', 'subscription_url', 'commits_url', 'git_commits_url', 'comments_url', 'issue_comment_url', 'contents_url', 'compare_url', 'merges_url', 'archive_url', 'downloads_url', 'issues_url', 'pulls_url', 'milestones_url', 'notifications_url', 'labels_url', 'releases_url', 'deployments_url', 'created_at', 'updated_at', 'pushed_at', 'git_url', 'ssh_url', 'clone_url', 'svn_url', 'homepage', 'size', 'stargazers_count', 'watchers_count', 'language', 'has_issues', 'has_projects', 'has_downloads', 'has_wiki', 'has_pages', 'forks_count', 'mirror_url', 'archived', 'disabled', 'open_issues_count', 
+# 'license', 'forks', 'open_issues', 'watchers', 'default_branch'])                
